0fa9e9628a3bfd49f770b1852abac805efda0b26
Yann Hallage <contact.devhllg@gmail.com>
2026-04-25 10:17:41 +0000

    update

--- src/app.ts
+++ src/app.ts
@@ -40,6 +40,7 @@ export function createApp(): express.Application {
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:4000",
+      "https://neo-ga.vercel.app",
       "http://localhost:4000",
     ].map(normalizeOrigin),
   );
